unkindjj/cardanokind

Split up Cardano.CLI.Shelley.Key and introduce Cardano.Api.DeserialiseAnyOf

cardano-cli/src/Cardano/CLI/Shelley/Run/Pool.hs
@@ -13,14 +13,14 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, handleIOExceptT
 
 import qualified Data.ByteString.Char8 as BS
 
+import           Cardano.Api.DeserialiseAnyOf (InputDecodeError)
 import           Cardano.Api.Typed
 
 import qualified Shelley.Spec.Ledger.Slot as Shelley
 
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (InputDecodeError, OutputDirection (..),
-                     VerificationKeyOrFile, readVerificationKeyOrFile,
-                     serialiseInputToBech32AndWrite)
+import           Cardano.CLI.Shelley.Key (OutputDirection (..), VerificationKeyOrFile,
+                     readVerificationKeyOrFile, serialiseInputToBech32AndWrite)
 
 
 data ShelleyPoolCmdError

zliu41/cardano-node

cardano-cli/src/Cardano/CLI/Shelley/Run/Pool.hs
@@ -14,14 +14,14 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, handleIOExceptT
 import qualified Data.ByteString.Char8 as BS
 
 import           Cardano.Api
+import           Cardano.Api.DeserialiseAnyOf (InputDecodeError)
 import           Cardano.Api.Shelley
 
 import qualified Shelley.Spec.Ledger.Slot as Shelley
 
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (InputDecodeError, OutputDirection (..),
-                     VerificationKeyOrFile, readVerificationKeyOrFile,
-                     writeOutputBech32)
+import           Cardano.CLI.Shelley.Key (OutputDirection (..), VerificationKeyOrFile,
+                     readVerificationKeyOrFile, writeOutputBech32)
 
 
 data ShelleyPoolCmdError