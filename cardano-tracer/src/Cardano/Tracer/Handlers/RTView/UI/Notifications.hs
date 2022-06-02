2e038418eec27c6a7ca3ea2c1b4ede75dcbf3829
Denis Shevchenko <denis.shevchenko@iohk.io>
2022-06-02 12:32:32 +0400

    enc done

--- cardano-tracer/src/Cardano/Tracer/Handlers/RTView/UI/Notifications.hs
+++ cardano-tracer/src/Cardano/Tracer/Handlers/RTView/UI/Notifications.hs
@@ -8,7 +8,7 @@ module Cardano.Tracer.Handlers.RTView.UI.Notifications
   , saveEmailSettings
   ) where
 
-import           Control.Exception.Extra (ignore)
+import           Control.Exception.Extra (ignore, try_)
 import           Control.Monad (unless)
 import           Crypto.Cipher.AES (AES256)
 import           Crypto.Cipher.Types (BlockCipher (..), cipherInit, ctrCombine, nullIV)
@@ -40,9 +40,19 @@ restoreEmailSettings window = readSavedEmailSettings >>= setEmailSettings
   setValue elValue elId =
     unless (null elValue || elValue == "-1") $
       findAndSet (set value elValue) window elId
-  
+
 saveEmailSettings :: Window -> UI ()
 saveEmailSettings window = do
+  settings <- getCurrentSettings window
+  liftIO . ignore $ do
+    (pathToEmailSettings, _) <- getPathsToNotificationsSettings
+    -- Encrypt JSON-content to avoid saving user's private data in "plain mode".
+    case encryptJSON . LBS.toStrict . encode $ settings of
+      Right encryptedJSON -> BS.writeFile pathToEmailSettings encryptedJSON
+      Left _ -> return ()
+
+getCurrentSettings :: Window -> UI EmailSettings
+getCurrentSettings window = do
   smtpHost  <- findAndGetValue window "es-smtp-host"
   smtpPort  <- findAndGetValue window "es-smtp-port"
   username  <- findAndGetValue window "es-username"
@@ -51,33 +61,29 @@ saveEmailSettings window = do
   emailFrom <- findAndGetValue window "es-email-from"
   emailTo   <- findAndGetValue window "es-email-to"
   subject   <- findAndGetValue window "es-subject"
-  let settings = EmailSettings
-        { esSMTPHost  = pack smtpHost
-        , esSMTPPort  = readInt (pack smtpPort) (-1)
-        , esUsername  = pack username
-        , esPassword  = pack password
-        , esSSL       = read ssl
-        , esEmailFrom = pack emailFrom
-        , esEmailTo   = pack emailTo
-        , esSubject   = pack subject
-        }
-  liftIO . ignore $ do
-    (pathToEmailSettings, _) <- getPathsToNotificationsSettings
-    -- Encrypt JSON-content to avoid saving user's private data in "plain mode".
-    case encryptJSON . LBS.toStrict . encode $ settings of
-      Right encryptedJSON -> BS.writeFile pathToEmailSettings encryptedJSON
-      Left _ -> return ()
+  return $ EmailSettings
+    { esSMTPHost  = pack smtpHost
+    , esSMTPPort  = readInt (pack smtpPort) (-1)
+    , esUsername  = pack username
+    , esPassword  = pack password
+    , esSSL       = read ssl
+    , esEmailFrom = pack emailFrom
+    , esEmailTo   = pack emailTo
+    , esSubject   = pack subject
+    }
 
 readSavedEmailSettings :: UI EmailSettings
 readSavedEmailSettings = liftIO $ do
   (pathToEmailSettings, _) <- getPathsToNotificationsSettings
-  encryptedSettings <- BS.readFile pathToEmailSettings
-  case decryptJSON encryptedSettings of
+  try_ (BS.readFile pathToEmailSettings) >>= \case
     Left _ -> return defaultSettings
-    Right jsonSettings ->
-      case decodeStrict' jsonSettings of
-        Nothing -> return defaultSettings
-        Just (settings :: EmailSettings) -> return settings
+    Right encryptedSettings ->
+      case decryptJSON encryptedSettings of
+        Left _ -> return defaultSettings
+        Right jsonSettings ->
+          case decodeStrict' jsonSettings of
+            Nothing -> return defaultSettings
+            Just (settings :: EmailSettings) -> return settings
  where
   defaultSettings = EmailSettings
     { esSMTPHost  = ""
@@ -99,8 +105,9 @@ encryptJSON plainJSON = ctrCombine
   cInit :: Either CryptoError AES256
   cInit = eitherCryptoError $ cipherInit key
 
+  -- The length must be exactly 32 bytes (256 bits).
   key :: BS.ByteString
-  key = "TRALIVALI"
+  key = "n3+d6^jrodGe$1Ljwt;iBtsi_mxzp-47"
 
 decryptJSON :: BS.ByteString -> Either CryptoError BS.ByteString
 decryptJSON = encryptJSON -- Encryption/decryption is symmetric.
